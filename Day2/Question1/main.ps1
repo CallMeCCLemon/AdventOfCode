60f17fc435d6f8e3b122f7f71fae9814fb3fdafe
CallMeCCLemon <tommy-gun101@hotmail.com>
2020-12-02 22:41:33 -0800

    Adding Day 2 Question 1 Solution

---
+++ Day2/Question1/main.ps1
@@ -0,0 +1,18 @@
+$ValidCount = 0
+foreach ($Line in Get-Content -Path ..\input.txt) {
+    
+    # Use Regex to get all of the components of the input string.
+    if ($Line -match '(?<LowerRange>\d+)-(?<UpperRange>\d+) (?<Letter>\w): (?<Password>\w+)') {
+        # Split on the target character then count how many splits were made (i.e. how many instances)
+        # of the character there were.
+        $LetterCount = $Matches.Password.split($Matches.Letter).Length - 1
+
+        # Check if the occurence count is within the range and increment the valid password count if so.
+        if (($LetterCount -ge $Matches.LowerRange) -and ($LetterCount -le $Matches.UpperRange)) {
+            Write-Host "Valid password!"
+            $ValidCount++
+        } 
+    }
+}
+
+Write-Host "Final Count: $ValidCount"
