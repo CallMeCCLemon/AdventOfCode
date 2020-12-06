6e6fcf06ef6bafb4161df9f27d89384e51f75b6b
Thomas Littlejohn <thomas.e.littlejohn@gmail.com>
2020-12-06 10:12:58 -0800

    Adding day 3 question 1 solution

---
+++ Day3/Question1/main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>                                                                                     
+#include <stdlib.h>
+
+const int LINE_LENGTH = 31;
+// const int LINE_LENGTH = 11; // For debugging with the provided solution.
+const int X_INCREMENT_PER_ROW = 3;
+const char TREE_CHAR = '#';
+char getCharAtIndex(FILE* inputFile, int targetIndex);
+
+int main() 
+{
+    FILE *inputFile = fopen("../input.txt", "r");
+    // FILE *inputFile = fopen("tmp.txt", "r"); // For debugging with the provided solution.
+    char firstLineChar[1];
+
+    int numberOfTreesHit = 0;
+    int currentX = 0;
+    int lineNumber = 1;
+    char lastCharacter;
+
+    while (lastCharacter != EOF)
+    {
+        printf("Line number: %d\n", lineNumber);
+        printf("Index = %d\n", currentX);
+        lastCharacter = getCharAtIndex(inputFile, currentX);
+        printf("Character: %c\n", lastCharacter);
+
+        currentX = (currentX + X_INCREMENT_PER_ROW) % LINE_LENGTH;
+        if (lastCharacter == TREE_CHAR)
+        {
+            numberOfTreesHit++;
+        }
+        lineNumber++;
+    }
+
+    fclose(inputFile);
+
+    printf("Number of trees hit: %d\n", numberOfTreesHit);
+    return 0;
+}
+
+/**
+ * Reads a line of a file and returns the desired character at the index.
+ * If the end of file was reached, returns EOF.
+ */
+char getCharAtIndex(FILE* inputFile, int targetIndex) 
+{
+    // Read the entire line.
+    char currentChar;
+    char targetChar;
+    int currentIndex = 0;
+
+    while (currentChar != '\n' && currentChar != EOF)
+    {
+        currentChar = fgetc(inputFile);
+
+        if (currentIndex == targetIndex)
+        {
+            targetChar = currentChar;
+        }
+
+        currentIndex++;
+    }
+    
+    // Return just the one character.
+    if (currentChar == EOF)
+    {
+        return EOF;
+    }
+    else
+    {
+        return targetChar;
+    }
+}
